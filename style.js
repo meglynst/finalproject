face38df0e357db16c946f1386d3f4b90e939eea
meglynst <meglynst@umich.edu>
2019-04-18 23:49:48 -0400

    Add files via upload

--- style.js
+++ style.js
@@ -1,3 +1,4 @@
+// HAMBURGER MENU
 function hamburgerNav() {
 	var hamburger = document.getElementById("hamburger-icon");
 	const nav = document.getElementById("myNav");
@@ -12,19 +13,22 @@ function hamburgerNav() {
 		// console.log(e.target)
 		if (navIsOpen) {
 			nav.style.display = "block";
-			navIsOpen = false;
+			navIsOpen = true;
 		} else {
 			nav.style.display = "none";
-			navIsOpen = true;
+			navIsOpen = false;
 		}
 	})
 }
+
+
+
+// STICKY NAV 
 
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myStickyNav()};
 
 // Get the navbar
-
 var navbar = document.querySelector("#myNav");
 
 // Get the offset position of the navbar
@@ -39,7 +43,57 @@ function myStickyNav() {
   }
 }
 
-// source from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_matchmedia
-var x = window.matchMedia("(max-width: 320px)")
-myStickyNav(x) // Call listener function at run time
-x.addListener(myStickyNav) // Attach listener function on state changes
+
+// ACCORDION
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+
+// MODAL POPUP
+// function myModalPls() {
+// // Get the modal
+// var mymodal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+// var mybtn = document.getElementById("modalBtn");
+
+// // Get the <span> element that closes the modal
+// var myspan = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal 
+// mybtn.onclick = function() {
+//   mymodal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// myspan.onclick = function() {
+//   mymodal.style.display = "none";
+// }
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == mymodal) {
+//     mymodal.style.display = "none";
+//   }
+// }
+
+
+
+// // source from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_matchmedia
+// var x = window.matchMedia("(max-width: 320px)")
+// myStickyNav(x) // Call listener function at run time
+// x.addListener(myStickyNav) // Attach listener function on state changes
